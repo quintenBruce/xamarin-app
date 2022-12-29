8f8fc50d42e763b2a912f3879a9e80789eb674b2
Quinten Bruce <qcbruce7@gmail.com>
2022-12-28 22:33:16 -0600

    Add file functions class to handle file CRUD operations

---
+++ App1/App1/Helpers/FileFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace App1.Helpers
+{
+    class FileFunctions
+    {
+        public static int DeleteFile (string path)
+        {
+            File.Delete(path);
+            return 1;
+
+
+        }
+    }
+}
