ccef02cc7244df087c563487ebac3668f6b8e996
Quinten Bruce <qcbruce7@gmail.com>
2022-12-31 13:11:46 -0600

    Add FileFunctions class to abstract file operations

--- App1/App1/Helpers/FileFunctions.cs
+++ App1/App1/Helpers/FileFunctions.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace App1.Helpers
 {
-    class FileFunctions
+    internal class FileFunctions
     {
-        public static int DeleteFile (string path)
+        //DeleteFile method will delete file given by the path parameter. Will return true if file was deleted successfully, else false
+        public static bool DeleteFile(string path)
         {
             File.Delete(path);
-            return 1;
 
+            if (File.Exists(path))
+                return false;
+            return true;
+        }
 
+        //WriteFile method will write the stream parameter to a new file given by the path parameter. Will return true if new file has length > 0, else false
+        public static async Task<bool> WriteFile(Stream stream, string path)
+        {
+            var newStream = File.OpenWrite(path);
+            await stream.CopyToAsync(newStream);
+
+            if (newStream.Length == 0)
+                return false;
+
+            return true;
         }
     }
 }
